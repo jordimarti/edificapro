38fcb68edd66d8aefaef4e7e0a6fd6cc7a6535db
Jordi Martí Muñoz <jordimarti@me.com>
2014-07-02 22:24:28 +0200

    Seed file modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
 
 Course.create(id: 1, name: 'Introducción al Passivhaus para climas cálidos', description: 'Curso de introducción para entender las bases del estándar Passivhaus', image: 'courses/c01.jpg', price: 5900, start_controller: 'c01', start_action: 'contexto')
 Course.create(id: 2, name: 'Auditorías energéticas en edificación', description: 'Curso práctico para la realización de auditorías energéticas en los edificios', image: 'courses/c02.jpg', price: 4900, start_controller: 'c02', start_action: 'introduccion')
